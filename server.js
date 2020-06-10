ae2c45dfde5243a97f42b5c55490008998e523b3
Anh Le <kunquan2345@gmail.com>
2020-06-10 13:11:05 -0700

    Implement some basic socket.io

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-const express = require('express')
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket) => {
+  //const url = socket.request.headers.referer;
+
+  socket.broadcast.emit("message", "New user just enter chat room");
+
+  socket.on("disconnect", () => io.emit("message", "User has leave the room"));
+});
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+server.listen(PORT, () => console.log(`Listen to port ${PORT}`));
